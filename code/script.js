4b986304372b4b8df40e8c57042e10eaf731534f
schouenkes <m.schouenke@gmail.com>
2024-09-12 13:04:58 +0200

    a function to generate seat buttons + styled welcome screen

--- code/script.js
+++ code/script.js
@@ -24,8 +24,42 @@ const createWelcomeScreen = () => {
   const welcomeText2 = document.createElement("p");
   welcomeText2.textContent = "Click on a seat and let us continue the booking.";
   introTextBox.appendChild(welcomeText2);
+
+  const buttonBox = document.createElement("div");
+  buttonBox.className = "movieseat-box";
+  buttonBox.id = "seatBox";
+  welcomeScreen.appendChild(buttonBox);
+
+  generateSeatButtons();
 };
 
-const generateSeatButtons = () => {};
+const generateSeatButtons = () => {
+  const movieSeatBox = document.getElementById("seatBox");
+  
+  movieSeatBox.innerHTML = "";
+
+  let movieSeatButton;
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth <= 380) {
+    movieSeatButton = 70;
+  } else if (screenWidth <= 480) {
+    movieSeatButton = 66;
+  } else if (screenWidth <= 600) {
+    movieSeatButton = 50;
+  } else {
+    movieSeatButton = 60;
+  }
+
+  for (i = 0; i < movieSeatButton; i++) {
+    const seatButton = document.createElement("button");
+    seatButton.className = "seat-button";
+    seatButton.type = "button";
+    seatButton.textContent = "x";
+    movieSeatBox.appendChild(seatButton);
+  }
+};
 
 showWelcomeScreen();
+
+window.addEventListener("resize", generateSeatButtons);
